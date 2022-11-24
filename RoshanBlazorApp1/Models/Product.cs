e36fa4c0578d554a96c646c3f741a7a56f787ed2
Roshan Gauchan <91730218+Roarsan@users.noreply.github.com>
2022-11-24 11:28:34 +0000

    Add project files.

---
+++ RoshanBlazorApp1/Models/Product.cs
@@ -0,0 +1,28 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace RoshanBlazorApp1.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+        [Required,StringLength(30)]
+
+        public string ShoeName { get; set; } = string.Empty;
+        [Required, StringLength(255)]
+        public string Colour { get; set; } = string.Empty;
+        [Required]
+        public decimal Price { get; set; }
+        [Required]
+        public int Size { get; set; }
+        [Required, StringLength(30)]
+        public string Material { get; set; } = string.Empty;
+
+        [Range(0,6)]
+        public int Type { get; set; } = 0;
+        [Required]
+        public decimal DiscountedPrice { get; set; }
+        [StringLength(30)]
+        public string ImageURL { get; set; } = string.Empty;    
+
+    }
+}
---
+++ RoshanBlazorApp1/Services/ProductService.cs
@@ -0,0 +1,27 @@
+﻿using RoshanBlazorApp1.Models;
+using System.Drawing;
+using System.Security.Cryptography.X509Certificates;
+
+namespace RoshanBlazorApp1.Services
+{
+    public class ProductService
+    {
+        public static List<Product> ProductList = new List<Product>()
+        {
+            new Product()
+            {
+                Id = 1,
+                ShoeName="Adidas Waffle",
+                Colour ="Black",
+                Price= 30,
+                Size= 9,
+                Type= 1,
+                Material="COTTON",
+                DiscountedPrice = 20,
+                ImageURL= "IMG.PNG"
+
+            }
+            
+        }
+    }
+}
